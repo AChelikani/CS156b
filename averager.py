91cea1c2b2b9f7d53fd254869c2ba58410aaa1e6
Jim Zhang <EonSin@users.noreply.github.com>
2018-04-15 21:18:25 -0700

    rewrote using pandas df but slow

--- averager.py
+++ averager.py
@@ -1,31 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr  7 18:20:51 2018
+Created on Sun Apr 15 02:30:32 2018
 
-@author: PITAHAYA
+@author: JIM
 """
 
+import sys
+import pandas as pd
 import numpy as np
 
-def create_matrix(n, m):
-    result = np.zeros((n,m))
-    with open("um/all.dta") as lines:
-        for line in lines:
-            user = int(line.split(' ')[0])
-            movie = int(line.split(' ')[1])
-            rating = int(line.split(' ')[3][0])
-            if (rating != 0):
-                result[user-1][movie-1] = rating
-                if not (user%10000):
-                    print(user, movie, rating)
-            else:
-                result[user-1][movie-1] = np.nan
-    return result
+#%% Sorter
 
-def find_mean(array, row, col):
-    row_mean = np.nanmean(array[row-1])
-    col_mean = np.nanmean(array.T[col-1])
-    return (row_mean + col_mean) / 2
+data_path = 'data/subset-user.txt'
+data_path = '../../um/all.dta'
 
-um_matrix = create_matrix(458293, 17770)
-print(find_mean(um_matrix, 5, 5))
+df = pd.read_csv(data_path, delimiter=' ', header=None)
+print(sys.getsizeof(df))
+df.columns = ['user', 'movie', 'date', 'rating']
+
+to_predict = df[df.rating==0]
+usable = df[df.rating!=0]
+del df
+
+#%% Calculator
+
+to_predict = to_predict.values.T[0:2].T
+predicted = np.zeros(len(to_predict))
+user_means = np.zeros(np.max(usable.user))
+movie_means = np.zeros(np.max(usable.movie))
+for i in range(len(to_predict)):
+    want_user = to_predict[i][0]
+    want_movie = to_predict[i][1]
+    
+    if user_means[want_user-1] != 0:
+        usermean = user_means[want_user-1]
+    else:
+        user_means[want_user-1] = np.mean(usable[usable.user==want_user].rating)
+        usermean = user_means[want_user-1]
+    
+    if movie_means[want_movie-1] != 0:
+        moviemean = movie_means[want_movie-1]
+    else:
+        movie_means[want_movie-1] = np.mean(usable[usable.movie==want_movie].rating)
+        moviemean = movie_means[want_movie-1]
+    
+    predicted[i] = (usermean + moviemean) / 2
+    if i % 1000 == 0:
+        print(i, 'of', len(to_predict))
+
+np.savetxt('results.dta', predicted, fmt='%.3f')
