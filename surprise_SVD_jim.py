806d547895c9e124242e1f9790ca48ece8bc3b5e
Jim <eonsin@126.com>
2018-04-19 00:46:42 -0700

    wrote import - model - training - prediction - output - model save contains progress reports and timestamps for easier management

---
+++ surprise_SVD_jim.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 16 22:51:43 2018
+
+@author: JIM
+"""
+
+from surprise import SVD, BaselineOnly, Reader, evaluate, Dataset, SlopeOne
+from surprise.model_selection import cross_validate
+import numpy as np
+import pandas as pd
+import time
+import pickle
+
+file_path = 'custom_data/training.csv'
+file_path_test = 'custom_data/test.csv'
+title = 'SVD_f100_ep100_lr005'
+
+print(time.asctime(), 'loading training data')
+df = pd.read_csv(file_path, delimiter=' ', header=None)
+df.columns = ['user', 'item', 'timestamp', 'rating']
+reader = Reader(rating_scale=(1, 5))
+data = Dataset.load_from_df(df[['user', 'item', 'rating']], reader)
+del df
+
+print(time.asctime(), 'loaded training data, now building trainset')
+trainset = data.build_full_trainset()
+del data
+
+print(time.asctime(), 'training set built, now training')
+# algo = SlopeOne()
+#
+#
+# MODEL DEFINITION
+algo = SVD(verbose=True, n_factors=100, n_epochs=100, lr_all=0.005)
+#
+#
+#
+algo.fit(trainset)
+
+# loads trained algo from pickle
+# with open('custom_data/<algo_name>.pickle', 'rb') as f:
+    # algo = pickle.load(f)
+print(time.asctime(), 'training complete, now loading prediction data')
+to_predict = pd.read_csv(file_path_test, delimiter=' ', header=None)
+to_predict = to_predict.values.T[0:2].T
+predicted = np.zeros(len(to_predict))
+
+print(time.asctime(), 'prediction data loaded, now predicting')
+for i in range(len(predicted)):
+    user = to_predict[i][0]
+    item = to_predict[i][1]
+    predicted[i] = algo.predict(uid=user, iid=item, verbose=0).est
+    if (i%500000 == 0):
+        print(i, 'of', len(predicted), 'predicted')
+
+print(time.asctime(), 'now saving predictions')
+np.savetxt('custom_data/'+title+'.dta', predicted, fmt='%.3f')
+
+# del trainset # idk if we can del trainset and still save algo yet
+print(time.asctime(), 'saved predictions, now saving trained model')
+del to_predict
+with open('custom_data/'+title+'.pickle', 'wb') as f:
+    pickle.dump(algo, f, pickle.HIGHEST_PROTOCOL)
+print(time.asctime(), 'done')
